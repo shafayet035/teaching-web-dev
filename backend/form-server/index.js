0025c7ca3f21ba3e9ba5219066903a085d25d504
shafayet035 <shafayet035@gmail.com>
2025-04-17 23:22:42 +0600

    Form-server

---
+++ backend/form-server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+const tweets = [];
+
+app.post('/tweet', (req, res) => {
+  const tweet = req.body;
+  tweets.push(tweet);
+
+  console.log('Tweet added to array!');
+
+  res.json({
+    success: true,
+    message: 'Tweet has been posted',
+  });
+});
+
+app.get('/tweets', (req, res) => {
+  res.json(tweets);
+});
+
+app.listen(9000, () => {
+  console.log('The server is running on http://localhost:9000');
+});

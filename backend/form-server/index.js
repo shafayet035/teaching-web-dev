e87bef044b627bce70036ccb23149d5fe029f6f9
shafayet035 <shafayet035@gmail.com>
2025-04-17 23:31:45 +0600

    Serving html file from node server

--- backend/form-server/index.js
+++ backend/form-server/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Server is running');
-});
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', (req, res) => {
+//   res.send('Server is running');
+// });
 
 const tweets = [];
 
